2561a137bf90a544a096c8f9d62114945f2a731e
Chaoran Chen <mail@chaoran-chen.de>
2022-10-18 15:19:49 +0200

    chore: delete unused code

--- R/plots/example_plot.R
+++
@@ -1,9 +0,0 @@
-library(ggplot2)
-
-
-#' @param data A tibble with the columns: date, count
-plot_example <- function(data) {
-  p <- ggplot(data, aes(x = date, y = count)) +
-    geom_bar(stat = "identity")
-  return(p)
-}
