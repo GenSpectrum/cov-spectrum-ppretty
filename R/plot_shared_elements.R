51f2d3c9cc4bc56a7cc95d443eb58f0205264632
Sarah Nadeau <nadeaus@bs-mbpr342.d.ethz.ch>
2022-11-09 10:37:03 +0000

    feat: improve diverging colors

--- R/plot_shared_elements.R
+++ R/plot_shared_elements.R
@@ -9,8 +9,21 @@ line_bar_size <- 1  # controls relative thickness of lines in line plots
 footnote <- "Plot by cov-spectrum.org enabled by data from GISAID"
 
 # Colors to use preferentially
-diverging_colors <- RColorBrewer::brewer.pal(n = 8, name = "Set2")
+# First color will be used for single-variable plots
+diverging_colors <- c(
+  '#353B89',
+  '#3B8935',
+  '#F0A21A',
+  '#CE1A39',
+  '#F7D233',
+  '#A21AF0',
+  '#21A0A1',
+  '#898335',
+  '#A76660',
+  '#B8D4DC',
+  '#F57E3B',
+  '#DB598A')
 
 # Function that will return as many colors as necessary from when there are more values than length(diverging_colors).
-# Needs to take argument 'n'.
+# Needs to take argument 'n' for number of colors to return.
 get_unlimited_colors <- terrain.colors
